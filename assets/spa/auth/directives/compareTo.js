367a2435a6c42119c0b25591dda7c8c1ba299136
Chad Lefort <chadlefort@gmail.com>
2015-04-23 00:50:55 -0500

    Started working on form validation.

---
+++ assets/spa/auth/directives/compareTo.js
@@ -0,0 +1,38 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('dabou.auth')
+    .directive('compareTo', compareTo);
+
+  compareTo.$inject = [];
+
+  /**
+   * @ngdoc directive
+   * @name dabou.auth.directive:compareTo
+   * @restrict
+   * @description
+   *
+   */
+  function compareTo() {
+    var directive = {
+      link: link,
+      restrict: 'EA',
+      require: 'ngModel',
+      scope: {
+        otherModelValue: '=compareTo'
+      }
+    };
+    return directive;
+
+    function link(scope, element, attrs, ngModel) {
+      ngModel.$validators.compareTo = function (modelValue) {
+        return modelValue == scope.otherModelValue;
+      };
+
+      scope.$watch("otherModelValue", function () {
+        ngModel.$validate();
+      });
+    }
+  }
+})();
