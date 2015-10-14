7871a0d4beb03d9a15fcbdaef13d2fd8f2084936
Chad Lefort <chadlefort@gmail.com>
2015-10-13 20:32:01 -0500

    Added redirects if you are authenticated and logged in for the login and register pages.

--- assets/spa/auth/controllers/register.js
+++ assets/spa/auth/controllers/register.js
@@ -1,52 +1,64 @@
-(function () {
-  'use strict';
+(function() {
+    'use strict';
 
-  angular
-    .module('dabou.auth')
-    .controller('RegisterController', RegisterController);
+    angular
+        .module('dabou.auth')
+        .controller('RegisterController', RegisterController);
 
-  /**
-   * @ngdoc controller
-   * @name dabou.auth.controller:RegisterController
-   * @description
-   *
-   */
-  RegisterController.$inject = ['_', 'urlS3Bucket', 'authService', '$state', 'toastr', 'globalData'];
+    /**
+     * @ngdoc controller
+     * @name dabou.auth.controller:RegisterController
+     * @description
+     *
+     */
+    RegisterController.$inject = ['_', 'urlS3Bucket', 'authService', '$state', 'toastr', 'globalData'];
 
-  function RegisterController(_, urlS3Bucket, authService, $state, toastr, globalData) {
-    var vm = this,
-      _csrf = globalData.tokenData._csrf;
+    function RegisterController(_, urlS3Bucket, authService, $state, toastr, globalData) {
+        var vm = this,
+            _csrf = globalData.tokenData._csrf,
+            status = globalData.userData.status;
 
-    // PUBLIC PROPERTIES
-    vm.title = 'Register';
-    vm.user = {};
-    vm.state = $state.current.name;
-    vm.urlS3Bucket = urlS3Bucket;
+        // PUBLIC PROPERTIES
+        vm.isLoggedIn = false;
+        vm.title = 'Register';
+        vm.user = {};
+        vm.state = $state.current.name;
+        vm.urlS3Bucket = urlS3Bucket;
 
-    // PUBLIC FUNCTIONS
-    vm.addLocalUser = addLocalUser;
+        // PUBLIC FUNCTIONS
+        vm.addLocalUser = addLocalUser;
 
-    // init
-    activate();
+        // init
+        activate();
 
-    // PRIVATE FUNCTIONS
-    function activate() {
-      vm.user = {_csrf: _csrf};
+        // PRIVATE FUNCTIONS
+        function activate() {
+            vm.user = {
+                _csrf: _csrf
+            };
+            authenticated();
+        }
+
+        function addLocalUser() {
+            authService.addLocalUser(vm.user)
+                .then(function(data) {
+                    if (data.error) {
+                        var errors = data.error;
+                        _.each(errors, function(value, key) {
+                            toastr.error(errors[key]);
+                        });
+                    } else {
+                        $state.go('account');
+                        toastr.success('Success.Passport.User.Created');
+                    }
+                })
+        }
+
+        function authenticated() {
+            if (status) {
+                vm.isLoggedIn = true;
+                $state.go('index');
+            }
+        }
     }
-
-    function addLocalUser() {
-      authService.addLocalUser(vm.user)
-        .then(function (data) {
-          if (data.error) {
-            var errors = data.error;
-            _.each(errors, function (value, key) {
-              toastr.error(errors[key]);
-            });
-          } else {
-            $state.go('account');
-            toastr.success('Success.Passport.User.Created');
-          }
-        })
-    }
-  }
 })();
