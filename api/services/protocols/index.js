723f7c915ef58f41d3ed7a45e4c0e7cac05bee50
Chad Lefort <chadlefort@gmail.com>
2015-10-13 21:38:40 -0500

    Removed protocols not planning on using.

--- api/services/protocols/index.js
+++ api/services/protocols/index.js
@@ -16,7 +16,5 @@ module.exports = {
     local: require('./local'),
     oauth: require('./oauth'),
     oauth2: require('./oauth2'),
-    openid: require('./openid'),
-    bearer: require('./bearer'),
-    cas: require('./cas')
+    bearer: require('./bearer')
 };
